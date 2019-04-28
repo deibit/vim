d6a7f8999040355b6eee10f8cb4315448c7b747b
David Garcia <david.garcianunez@telefonica.com>
2019-04-28 19:59:39 +0200

    delete .vimrc, as .vim/vimrc make the work

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
+# Since Vim 7.4 and above the $HOME/.vimrc can be used as $HOME/.vim/vimrc
+# which is a more portable solution
 # execute from $HOME/vim when you are done with git clone
-cd ..
-mv vim .vim
-ln -s .vim/vimrc .vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #Neovim
